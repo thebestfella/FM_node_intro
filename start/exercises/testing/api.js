1b969e6eed0e0ae29c93ae4f6f1d4d51c98a2c13
thebestfella <thisispeter@msn.com>
2020-04-16 17:32:36 -0700

    finished almost everything except except express doesn't work as expected, need to fix after taking express course

--- start/exercises/testing/api.js
+++ start/exercises/testing/api.js
@@ -1,24 +1,35 @@
-const express = require('express')
-const morgan = require('morgan')
-const { urlencoded, json } = require('body-parser')
-const users = require('./users')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const { urlencoded, json } = require("body-parser");
+const users = require("./users");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(urlencoded({extended: true}))
-app.use(json())
+app.use(morgan("dev"));
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-app.get('/user/:id', async (req, res) => {
-  const id = req.id
+console.log("heyhey");
+
+app.get("/user/:id", async (req, res) => {
+  debugger;
+  //node --inspect index.js
+  //chrome://inspect
+
+  const id = req.param.id;
+  console.log(req.param.id);
   // should ge user by given id in route param
-  const user = await users.findUser(user => user.id === id)
-  res.status(200).send(user)
-})
+  const user = await users.findUser((user) => user.id === id);
+  res.status(200).send(user);
+});
 
-app.delete('/user/:id', async (req, res) => {
-  const id = req.id
-  await users.deleteUser(id)
-  res.status(201).send({id})
-})
+console.log("heyhey2");
 
-module.exports = app
+app.delete("/user/:id", async (req, res) => {
+  const id = req.id;
+  await users.deleteUser(id);
+  res.status(201).send({ id });
+});
+
+console.log("heyhey3");
+
+module.exports = app;
--- start/exercises/testing/index.js
+++ start/exercises/testing/index.js
@@ -1,5 +1,5 @@
-const app = require('./api')
+const app = require("./api");
 
-app.listen(3001, () => {
-  console.log(`server on @http://localhost:3001`)
-})
+app.listen(3002, () => {
+  console.log(`server on @http://localhost:3002`);
+});
